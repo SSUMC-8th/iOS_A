ba7812d76467283c5ebd9ae0f3265c5aed8daab0
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2025-03-20 18:30:28 +0900

    [check/week1] 1주차 과제

---
+++ Check/StarBucks/Sources/Modules/Login/Models/LoginModel.swift
@@ -0,0 +1,24 @@
+//
+//  LoginModel.swift
+//  Week1
+//
+//  Created by LEE on 3/17/25.
+//
+import Foundation
+
+
+struct LoginModel {
+  
+    private var id: String?
+    
+    private var password: String?
+    
+    var getId: String? {
+        return id
+    }
+    
+    var getPassword: String? {
+        return password
+    }
+    
+}
